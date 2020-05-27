43d5e56d00fde7f330e76cc310091209ed3430fb
Seraphine <chi811008@gmail.com>
2020-05-27 13:06:22 +0800

    text_type

--- hello.py
+++ hello.py
@@ -1,5 +1,3 @@
-import json
-
 from flask import Flask, request, abort
 
 from linebot import (
@@ -9,19 +7,26 @@ from linebot.exceptions import (
     InvalidSignatureError
 )
 from linebot.models import (
-    MessageEvent, PostbackEvent,
+    ImageSendMessage, MessageEvent, PostbackEvent,
     TextSendMessage, TemplateSendMessage,
     TextMessage, ButtonsTemplate,
     PostbackTemplateAction, MessageTemplateAction,
-    URITemplateAction, StickerSendMessage,
-    CarouselTemplate, CarouselColumn,
+    URITemplateAction, StickerSendMessage
 )
 
 app = Flask(__name__)
 
-
 line_bot_api = LineBotApi('K9VIeDOz0f7pg/kQMrqggOjd5rOcLwMYOg2PlrBJuQDpX3p1Q/o4+cuK6VapoA5q+j0QLdxZwwLu8as9S3Hi4gblljWUIEWAFG7i/4YzoEPBovw6yb6h2bZrLdeyB++yb4WTtzqSOJmPSPszvo2KwAdB04t89/1O/w1cDnyilFU=')
 handler = WebhookHandler('e365002afec95950d148063ee819297f')
+
+@app.route('/')
+def hello_world():
+    return 'Hello, World! My name is Seraphine. I am happy now'
+
+@app.route('/test')
+def test_page():
+    return 'In test page!'
+
 
 
 @app.route("/callback", methods=['POST'])
@@ -31,7 +36,6 @@ def callback():
 
     # get request body as text
     body = request.get_data(as_text=True)
-
     app.logger.info("Request body: " + body)
 
     # handle webhook body
@@ -42,43 +46,24 @@ def callback():
 
     return 'OK'
 
-@handler.add(MessageEvent, message=TextMessage)
-def pixabay_isch(event):
 
-    if event.source.user_id != "Udeadbeefdeadbeefdeadbeefdeadbeef":
-        try:
-            url = f"https://pixabay.com/images/search/{urllib.parse.urlencode({'q':event.message.text})[2:]}/"
-            headers = {'User-Agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/76.0.3809.132 Safari/537.36'}
+@handler.add(MessageEvent, message=TextMessage)
+def handle_message(event):
+    if event.message.text == "文字":
+        line_bot_api.reply_message(event.reply_token,TextSendMessage(text=event.message.text))
+    elif event.message.text == "Yes":
+        line_bot_api.reply_message(event.reply_token,StickerSendMessage(package_id=11539, sticker_id=52114123))
+    elif event.message.text == "OK":
+        line_bot_api.reply_message(event.reply_token,StickerSendMessage(package_id=11539, sticker_id=52114113))
+    elif event.message.text == "圖片":
+        pic = "https://s.yimg.com/ny/api/res/1.2/12UU2JphAsbxTTDca.7QFQ--~A/YXBwaWQ9aGlnaGxhbmRlcjtzbT0xO3c9MTA4MDtoPTcxNg--/https://media-mbst-pub-ue1.s3.amazonaws.com/creatr-uploaded-images/2019-11/7b5b5330-112b-11ea-a77f-7c019be7ecae"
+        line_bot_api.reply_message(event.reply_token,ImageSendMessage(original_content_url= pic, preview_image_url= pic))
         
-            req = urllib.request.Request(url, headers = headers)
-            conn = urllib.request.urlopen(req)
-        
-            print('fetch page finish')
-        
-            pattern = 'img srcset="\S*\s\w*,'
-            img_list = []
-        
-            for match in re.finditer(pattern, str(conn.read())):
-                img_list.append(match.group()[12:-3])
-            
-            random_img_url = img_list[random.randint(0, len(img_list)+1)]
-            print('fetch img url finish')
-            print(random_img_url)
-        
-            line_bot_api.reply_message(
-                event.reply_token,
-                ImageSendMessage(
-                    original_content_url=random_img_url,
-                    preview_image_url=random_img_url
-                )
-            )
-    # 如果找不到圖，就學你說話
-        except:
-            line_bot_api.reply_message(
-                event.reply_token,
-                TextSendMessage(text=event.message.text)
-            )
-            pass
+    else:
+        line_bot_api.reply_message(event.reply_token,TextSendMessage(text=event.message.text))
+
+
+
 
 
 if __name__ == "__main__":
