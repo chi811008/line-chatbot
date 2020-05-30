586b89b59dc89bb3a5a16ac496d68c4d9f5bbbd1
Seraphine <chi811008@gmail.com>
2020-05-30 18:18:38 +0800

    exceptions

--- hello.py
+++ hello.py
@@ -149,7 +149,7 @@ def search_info(event):
         line_bot_api.reply_message(
             event.reply_token,
             TextSendMessage(
-                text = text
+                text = search
                 )
             )
 
