b2bd81932ee112224a0b17d123f8c4c2e8f59fd3
Seraphine <chi811008@gmail.com>
2020-05-30 13:23:58 +0800

    search_name_change

--- hello.py
+++ hello.py
@@ -19,7 +19,7 @@ app = Flask(__name__)
 line_bot_api = LineBotApi('K9VIeDOz0f7pg/kQMrqggOjd5rOcLwMYOg2PlrBJuQDpX3p1Q/o4+cuK6VapoA5q+j0QLdxZwwLu8as9S3Hi4gblljWUIEWAFG7i/4YzoEPBovw6yb6h2bZrLdeyB++yb4WTtzqSOJmPSPszvo2KwAdB04t89/1O/w1cDnyilFU=')
 handler = WebhookHandler('e365002afec95950d148063ee819297f')
 
-def get_mountain(mountain):
+def get_database_connection():
     # Query
     import os
     import psycopg2
@@ -28,6 +28,23 @@ def get_mountain(mountain):
 
     conn = psycopg2.connect(DATABASE_URL, sslmode='require')
     cursor = conn.cursor()
+    return cursor
+
+def get_mountain_name(mountain):
+    cursor = get_database_connection()
+    postgres_select_query = f"""SELECT * FROM mountain"""
+
+    dic_moun_name = {}
+    cursor.execute(postgres_select_query)
+    for item in cursor.fetchall():
+    dic_moun_name[item[1]] = item
+
+    for name in dic_moun_name.keys():
+        if mountain in name:
+            return name
+
+def get_mountain(mountain):
+    cursor = get_database_connection()
 
     postgres_select_query = f"""SELECT * FROM mountain"""
 
@@ -124,8 +141,8 @@ def search_info(event):
         picture_url = get_mountain_picture(search)
         button_template_message = ButtonsTemplate(
             thumbnail_image_url=picture_url,
-            title=search,
-            text='請選擇',
+            title = get_mountain_name(search),
+            text = '請選擇',
             actions=[
                 PostbackTemplateAction(
                     label='山的資訊',
