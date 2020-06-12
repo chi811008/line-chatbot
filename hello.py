e477ea853c0ec9604dd3dd78e891feb13e6d68e1
Seraphine <chi811008@gmail.com>
2020-06-12 18:04:41 +0800

    search_篩選_postbacksearch_OK

--- hello.py
+++ hello.py
@@ -779,7 +779,7 @@ def search_info(event):
             # ),
         )
        
-        message = FlexSendMessage(alt_text="山的資訊", contents=CarouselContainer(contents=bubble))
+        message = FlexSendMessage(alt_text="山的資訊", contents=bubble)
         line_bot_api.reply_message(
             event.reply_token,
             message
